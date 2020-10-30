b38d6907de6fec4643606d9f410c4a67e113da15
ajasurbek <jasurbekashurbekov@gmail.com>
2020-10-30 20:14:39 +0500

    initial commit

--- js/js.js
+++ js/js.js
@@ -1,80 +1,30 @@
-// TAKRORLASH QISMI
-
-// var elExchangeForm = document.querySelector('.exchange-form');
-
-// elExchangeForm.addEventListener('submit', function (evt) {
-//   evt.preventDefault();
-
-//   var inputSum = parseFloat(elExchangeForm.querySelector('.input-summ').value.trim(), 10).toFixed(2);
-
-//   console.log(inputSum);
-// });
-
-// elExchangeForm.addEventListener('submit', function(evt) {
-//   evt.preventDefault();
-
-//   document.body.style.backgroundColor = 'orange';
-// });
+var ElForm = document.querySelector('.form');
+var ElInput = ElForm.querySelector('.form-input');
+var ElBtn = ElForm.querySelector('.main-button');
+var ElResult = document.querySelector('.game-text');
 
 
-// =================================
-// DARS QISMI
-// =================================
-
-// Find all necessary elements/Hamma kerakli elementlarni topamiz
-var elExchangeForm = document.querySelector('.exchange-form');
-var elSummInput = elExchangeForm.querySelector('.summ-input');
-var elCurrencySelect = elExchangeForm.querySelector('.currency-select');
-var elResultWrapper = elExchangeForm.querySelector('.result-wrapper');
-var elResult = elExchangeForm.querySelector('.result');
-var elAlert = elExchangeForm.querySelector('.alert-result');
-var elAlertMinus = elExchangeForm.querySelector('.alert-minus');
-
-// var sayHi = function (name) {
-//   console.log(`Hi, ${name}`);
-// };
-// sayHi('Muhammad');
-// document.querySelector('.btn-success').addEventListener('click', sayHi);
-
-
-var convertCurrencyToUzs = function (evt) {
+ElForm.addEventListener('submit' , function(evt){
   evt.preventDefault();
+  var ElForma = parseFloat(ElInput.value.trim() , 10);
 
-  var summ = parseFloat(elSummInput.value.trim(), 10);
 
-  if (isNaN(summ)) {
-    // console.log('Brat, son kiriting');
-    elAlert.classList.remove('d-none');
-    elResultWrapper.classList.add('d-none');
-    elAlertMinus.classList.add('d-none');
+
+  if( ElForma % 3 === 0 && ElForma % 5 === 0 ){
+    ElResult.textContent = "FizzBuzz";
+    return;
+  }
+  if( ElForma % 3 === 0){
+    ElResult.textContent = "Fizz";
+    return;
+  }
+  if( ElForma % 5 === 0){
+    ElResult.textContent = "Buzz";
+    return;
+  }
+  if( ElForma % 3 != 0 || ElForma % 5 != 0 ){
+    ElResult.textContent = "Error";
     return;
   }
 
-  if (summ < 0) {
-    elAlertMinus.classList.remove('d-none');
-    elAlert.classList.add('d-none');
-    elResultWrapper.classList.add('d-none');
-    return;
-  }
-
-  var currencyRate = parseFloat(elCurrencySelect.value.trim(), 10);
-
-  var result = (summ * currencyRate).toFixed(2);
-
-  elResultWrapper.classList.remove('d-none');
-  elAlert.classList.add('d-none');
-  elAlertMinus.classList.add('d-none');
-  elResult.textContent = result;
-};
-
-elExchangeForm.addEventListener('submit', convertCurrencyToUzs);
-
-
-var multiply = function (birinchiRakam, ikinchiRakam) {
-  // console.log(birinchiRakam * ikinchiRakam);
-  console.log('Men returndan oldinman');
-  return birinchiRakam * ikinchiRakam;
-};
-
-// ============
-
+})
